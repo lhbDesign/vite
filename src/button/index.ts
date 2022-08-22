0b93ed9b9fb4b770f011d40dedb0da9c4c143022
lihaibo <lihaibo@okay.cn>
2022-08-22 19:02:03 +0800

    feat: update

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,7 +1,7 @@
 import Button from "./Button";
 
 Button.install = (App) => {
-  App.component(Button.name,Button)
-}
+  App.component(Button.name, Button);
+};
 // 导出Button组件
 export default Button;
